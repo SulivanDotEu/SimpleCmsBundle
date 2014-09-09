1cb531ba5ce1c2fdd8bb1c546c90ec682c11ab3c
unknown <bel@bepark.be>
2014-09-09 09:06:53 +0200

    some changes on CountryStrategyRelation

--- Entity/CountryStrategyRelation.php
+++ Entity/CountryStrategyRelation.php
@@ -10,7 +10,6 @@ namespace Walva\SimpleCmsBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Walva\SimpleCmsBundle\Interfaces\Entity\ContentRequestInterface;
-use Walva\SimpleCmsBundle\Interfaces\Entity\StrategyDelivererRelation;
 use Walva\SimpleCmsBundle\Interfaces\View\TreeViewConfigurator;
 use Walva\SimpleCmsBundle\Interfaces\View\TreeViewInterface;
 use Walva\SimpleCmsBundle\View\Label;
@@ -21,7 +20,7 @@ use Walva\SimpleCmsBundle\View\Label;
  * @ORM\Table()
  * @ORM\Entity()
  */
-class CountryStrategyRelation implements StrategyDelivererRelation, TreeViewConfigurator
+class CountryStrategyRelation implements \Walva\SimpleCmsBundle\Interfaces\Entity\StrategyDelivererRelation, TreeViewConfigurator
 {
 
     /**
